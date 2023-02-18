89d13815ef3034373b905b4ecd2a76b09e7d6498
Hemantkonda <115872236+Hemantkonda@users.noreply.github.com>
2023-02-18 22:59:28 +0530

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $(".service-slide").owlCarousel({
         autoplay: true,
@@ -9,82 +9,141 @@ $(document).ready(function() {
         dots: true,
         loop: true,
         responsive: {
-            0: { items: 1 },
-            480: { items: 2 },
-            575: { items: 2 },
-            768: { items: 2 },
-            991: { items: 4 },
-            1200: { items: 4 }
-        }
-    
-    });
-    
-    });
-
-    $(document).ready(function() {
-
-        $(".image-slide").owlCarousel({
-            autoplay: true,
-            autoplayTimeout: 2000,
-            autoplayHoverPause: false,
-            items: 1,
-            nav: false,
-            dots: true,
-            loop: true,
-            responsive: {
-                0: { items: 1 },
-                480: { items: 1 },
-                575: { items: 1 },
-                768: { items: 1 },
-                991: { items: 1 },
-                1200: { items: 1 }
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            575: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            991: {
+                items: 4
+            },
+            1200: {
+                items: 4
             }
-        
-        });
-        
-        });
+        }
 
-        $(document).ready(function() {
+    });
 
-            $(".Carousel").owlCarousel({
-                autoplay: true,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: false,
-                items: 1,
-                nav: false,
-                dots: true,
-                loop: true,
-                responsive: {
-                    0: { items: 1 },
-                    480: { items: 1 },
-                    575: { items: 1 },
-                    768: { items: 1 },
-                    991: { items: 1 },
-                    1200: { items: 1 }
-                }
-            
-            });
-            
-            });
-    
-             function reveal() {
-                var reveals = document.querySelectorAll(".reveal");
-              
-                for (var i = 0; i < reveals.length; i++) {
-                  var windowHeight = window.innerHeight;
-                  var elementTop = reveals[i].getBoundingClientRect().top;
-                  var elementVisible = 100;
-              
-                  if (elementTop < windowHeight - elementVisible) {
-                    reveals[i].classList.add("active");
-                  } else {
-                    reveals[i].classList.remove("active");
-                  }
-                }
-              }
-              
-              window.addEventListener("scroll", reveal);
+});
 
-             
+$(document).ready(function () {
 
-  
+    $(".image-slide").owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
+        items: 1,
+        nav: false,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            575: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            991: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+
+    });
+
+});
+
+$(document).ready(function () {
+
+    $(".Carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        items: 1,
+        nav: false,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            575: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            991: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+
+    });
+
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+window.onscroll = function() {myFunction()};
+
+let header = document.getElementById("myHeader")
+let sticky = header.offsetTop;
+
+function myFunction(){
+    if (window.pageYOffset > sticky) {
+        header.classList.add("bg-white")
+    } else {
+        header.classList.remove("bg-white");
+      }
+}
+
+// window.onscroll = function() {myFunction()};
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
